2a37c7400298f11fe36f7a498cc02c58b457e86a
foyez <foyezar@yahoo.com>
2020-03-28 20:37:44 +0600

    add home route in logo

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { auth, facebookProvider } from '../../firebase/firebase.utils'
 
 import './Navigation.scss'
+import { Link } from 'react-router-dom'
 
 const Navigation = ({ currentUser }) => {
   // console.log(currentUser)
@@ -13,7 +14,9 @@ const Navigation = ({ currentUser }) => {
 
   return (
     <div className="d-flex justify-content-between align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
-      <h5 className="my-0 mr-md-auto font-weight-normal">Fact Checker</h5>
+      <h5 className="my-0 mr-md-auto font-weight-normal">
+        <Link to="/">Fact Checker</Link>
+      </h5>
       {!currentUser ? (
         <button className="btn btn-fb" onClick={signInWithFacebook}>
           Sign in with facebook
